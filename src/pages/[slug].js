c0e4046ebac5686785146410c25430c506f3a84c
webbywolf <info.webbywolf@gmail.com>
2023-01-29 22:54:16 +0530

    first comit

---
+++ src/pages/[slug].js
@@ -0,0 +1,137 @@
+import { Helmet } from 'react-helmet';
+
+import { getPostBySlug } from 'lib/posts';
+import { ArticleJsonLd } from 'lib/json-ld';
+import { helmetSettingsFromMetadata } from 'lib/site';
+import useSite from 'hooks/use-site';
+import usePageMetadata from 'hooks/use-page-metadata';
+
+import Layout from 'components/Layout';
+import Header from 'components/Header';
+import Section from 'components/Section';
+import Container from 'components/Container';
+import Content from 'components/Content';
+import Metadata from 'components/Metadata';
+import FeaturedImage from 'components/FeaturedImage';
+
+import styles from 'styles/pages/Post.module.scss';
+import { useEffect } from 'react';
+import Router from 'next/router';
+
+export default function Post({ post }) {
+  const { title, metaTitle, description, content, date, author, categories, featuredImage, isSticky = false } = post;
+
+  const { metadata: siteMetadata = {} } = useSite();
+
+  if (!post.og) {
+    post.og = {};
+  }
+
+  if (featuredImage?.sourceUrl) {
+    post.og.imageUrl = featuredImage.sourceUrl;
+    post.og.imageSecureUrl = post.og.imageUrl;
+    post.og.imageWidth = 2000;
+    post.og.imageHeight = 1000;
+  }
+
+  const { metadata } = usePageMetadata({
+    metadata: {
+      ...post,
+      title: metaTitle,
+      description: description || post.og?.description || `Read more about ${title}`,
+    },
+  });
+
+  if (process.env.WORDPRESS_PLUGIN_SEO !== true) {
+    metadata.title = `${title} - ${siteMetadata.title}`;
+    metadata.og.title = metadata.title;
+    metadata.twitter.title = metadata.title;
+  }
+
+  const metadataOptions = {
+    compactCategories: false,
+  };
+
+  const helmetSettings = helmetSettingsFromMetadata(metadata);
+
+  useEffect(() => {
+    Router.push(process.env.WORDPRESS_GRAPHQL_ENDPOINT.split('/graphql')[0] + Router.asPath);
+  }, []);
+
+  return (
+    <Layout>
+      <Helmet {...helmetSettings} />
+
+      <ArticleJsonLd post={post} siteTitle={siteMetadata.title} />
+
+      <Header>
+        {featuredImage && (
+          <FeaturedImage
+            {...featuredImage}
+            src={featuredImage.sourceUrl}
+            dangerouslySetInnerHTML={featuredImage.caption}
+          />
+        )}
+        <h1
+          className={styles.title}
+          dangerouslySetInnerHTML={{
+            __html: title,
+          }}
+        />
+        <Metadata
+          className={styles.postMetadata}
+          date={date}
+          author={author}
+          categories={categories}
+          options={metadataOptions}
+          isSticky={isSticky}
+        />
+      </Header>
+
+      <Content>
+        <Section>
+          <Container>
+            <div
+              className={styles.content}
+              dangerouslySetInnerHTML={{
+                __html: content,
+              }}
+            />
+          </Container>
+        </Section>
+      </Content>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params = {} } = {}) {
+  const { post } = await getPostBySlug(params?.slug);
+
+  if (!post) {
+    return {
+      props: {},
+      notFound: true,
+    };
+  }
+
+  const props = {
+    post,
+  };
+
+  return {
+    props,
+  };
+}
+
+export async function getStaticPaths() {
+  // Only render the most recent posts to avoid spending unecessary time
+  // querying every single post from WordPress
+
+  // Tip: this can be customized to use data or analytitcs to determine the
+  // most popular posts and render those instead
+
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+}
