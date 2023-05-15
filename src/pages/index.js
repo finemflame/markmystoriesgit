1b55373403ebf6bd37fb03b9ad3b621f3b70ebdb
webbywolf <info.webbywolf@gmail.com>
2023-05-16 00:21:41 +0530

    final touch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        background: '#202020',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>MarkMyStories</h1>
+    </div>
+  );
 }
