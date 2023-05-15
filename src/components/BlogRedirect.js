f69db5b7679d298333b41e2bdefa3f481dbf2291
webbywolf <info.webbywolf@gmail.com>
2023-05-16 00:12:44 +0530

    minor test

--- src/components/BlogRedirect.js
+++ src/components/BlogRedirect.js
@@ -4,7 +4,7 @@ export default function BlogRedirect() {
   setTimeout(() => {
     window.location =
       process.env.WORDPRESS_GRAPHQL_ENDPOINT.split('/graphql')[0] + window.location.pathname + window.location.search;
-  }, 500);
+  }, 10);
 
   return <></>;
 }
